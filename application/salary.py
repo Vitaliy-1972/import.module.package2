76faa9010e443d401cdd2bd018ab18281edce583
Vitaliy Falaleev <falaleev72@yandex.ru>
2024-06-09 18:25:47 +0300

    Initial commit

---
+++ application/salary.py
@@ -0,0 +1,2 @@
+def calculate_salary():
+    print('Зарплата 1000 rub')
---
+++ main.py
@@ -0,0 +1,8 @@
+from application.salary import calculate_salary
+from application.db.peoply import get_employees
+import datetime
+
+
+if __name__ == '__main__':
+    calculate_salary()
+    get_employees()
